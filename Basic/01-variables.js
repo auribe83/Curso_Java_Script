7d871198dced4d48e6295a2f4584a216fb9f486d
auribe83 <argenis429@gmail.com>
2025-02-14 17:17:20 -0400

    new file: variables.js

---
+++ Basic/01-variables.js
@@ -0,0 +1,27 @@
+// VARIABLES //
+
+// var
+console.log(helloworld)
+
+var helloworld = "!Hello, JavaScript!"
+console.log(helloworld)
+
+helloworld = "!Hola de nuevo, JavaScript!"
+console.log(helloworld)
+
+// let este tipo de variables solo son accesibles dentro de un bloque de codigo (ambito de variables)
+//console.log(helloworld2)  // ReferenceError: Cannot access 'helloworld2' before initialization
+
+let helloworld2 = "!Hola, JavaScript 2!"
+console.log(helloworld2)
+
+helloworld2 = "!Hola de nuevo, JavaScript 2!"
+console.log(helloworld2)
+
+// const
+const helloworld3 = "!Hola, JavaScript 3!"
+console.log(helloworld3)
+
+//TypeError: Assignment to constant variable, no se puede cambiar el valor de una constante.
+//helloworld3 = "!Hola de nuevo, JavaScript 3!"  
+//console.log(helloworld3)
