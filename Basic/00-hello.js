dbf82c14c55bc558a12416f3461b95f0cb7655a9
auribe83 <argenis429@gmail.com>
2025-02-14 17:16:52 -0400

    upt file: hello.js

--- Basic/00-hello.js
+++ Basic/00-hello.js
@@ -1,1 +1,14 @@
-console.log('Hola Mundo')
+// Esto es un comentario simple
+
+ /*
+ Esto es 
+ un comentario 
+ en varias lineas
+ */
+
+console.log("Hola JavaScript!")
+console.log('Hola JavaScript!')
+
+console.log(5)
+console.log("5.5")
+console.log(5 + 2)
