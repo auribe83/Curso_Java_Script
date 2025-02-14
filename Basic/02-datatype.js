7dd045e578dfeddd7c24c61bd8d5a392a8e91d42
auribe83 <argenis429@gmail.com>
2025-02-14 17:24:43 -0400

    new file: datatype.js

---
+++ Basic/02-datatype.js
@@ -0,0 +1,27 @@
+// Tipo de datos 
+
+// String (cadena de texto)
+let name =  'Argenis Uribe'
+let alias = "auribe83"
+let email = 'argenis429@gmail.com'
+
+// Int, float (numero enteros y decimal)
+let age = 42
+let height = 1.77 //Decimal
+
+// Boolean (Booleanos)
+let isTeacher = true
+let isStudent = false
+
+// Undefined Tipo de dato primitivo (Variable que se declara pero no se inicializa)
+let undefinedValue
+console.log(undefinedValue)
+
+//Tipo de Dato Null
+let nullValue = null
+
+//Tipo de Dato Symbol
+let mySymbol = Symbol("mysymbol")
+
+//Tipo de Dato Symbol
+let myBigint = BigInt()
