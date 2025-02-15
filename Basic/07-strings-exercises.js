bf1ba942a1ddaeaddca4ccf482cedad3e6698c33
auribe83 <argenis429@gmail.com>
2025-02-14 21:26:22 -0400

    new file: exercises Strings.js

---
+++ Basic/07-strings-exercises.js
@@ -0,0 +1,19 @@
+// 1. Concatena dos cadenas de texto.
+
+// 2. Muestra la longitud de una cadena de texto.
+
+// 3. Muestra el primer y ultimo caracter de un string.
+
+// 4. Convierte a mayusculas y minusculas un string.
+
+// 5. Crea una cadena de texto en varias lineas.
+
+// 6. Interpola el valor de una variable en un string.
+
+// 7. Reemplaza todos los espacios en blanco de un string por guiones.
+
+// 8. Comprueba si una cadena de texto contiene una palabra concreta.
+
+// 9. Comprueba si dos strings son iguales.
+
+// 10. Comprueba si dos strings tienen la misma longitud.
