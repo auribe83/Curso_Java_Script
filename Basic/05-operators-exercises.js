f6a2bf87916e9f567bb10e70b011cb24b7fb1c52
auribe83 <argenis429@gmail.com>
2025-02-14 20:37:41 -0400

    new file: exercises-operators

---
+++ Basic/05-operators-exercises.js
@@ -0,0 +1,20 @@
+// 1. Crea una variable para cada operación aritmetica.
+
+// 2. Crea una variable para cada tipo de operación de asignación, que haga uso de las variables utilizadas 
+//    para lasoperaciones aritmeticas.
+
+// 3. Imprime 5 comparaciones verdades con diferentes operadores de comparacion.
+
+// 4. Imprime 5 comparaciones falsas con diferentes operadores de comparacion.
+
+// 5. Utiliza el operador logico and.
+
+// 6. Utiliza el operador logico or.
+
+// 7. Combina ambos operadores lógicos.
+
+// 8. Añade alguna negacion.
+
+// 9. Utiliza el operador ternario.
+
+// 10. Combina operadores aritmeticos, de comparación y lógicos.
